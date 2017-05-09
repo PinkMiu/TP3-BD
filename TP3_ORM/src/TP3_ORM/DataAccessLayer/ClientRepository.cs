253b504d7132e8560028fd4c8a1590f8241f8ee7
Rémikya <remikya.hellal@gmail.com>
2017-05-09 17:02:56 -0500

    Dev
    Dev repository methods
    Dev EntityExists

---
+++ TP3_ORM/src/TP3_ORM/DataAccessLayer/ClientRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP3_ORM.Entities;
+using TP3_ORM.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TP3_ORM.DataAccessLayer
+{
+    public class ClientRepository<T> : IRepository<T> where T: Entity
+    {
+        private readonly DbContext _context;
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+        public void Cascade(T entity) { }
+    }
+}
---
+++ TP3_ORM/src/TP3_ORM/Services/EntityExists.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP3_ORM.DataAccessLayer;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TP3_ORM.Services
+{
+    public class EntityExists : Controller
+    {
+        private readonly OurDbContext _context;
+
+        public bool ClientExists(string codeClient)
+        {
+            var client = _context.Clients.FirstOrDefault(x => x.CODE_CLIENT == codeClient);
+            if (client != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ArtisteExists(int numeroArtiste)
+        {
+            var artiste = _context.Artistes.FirstOrDefault(x => x.NUMERO_ARTISTE == numeroArtiste);
+            if (artiste != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool GroupeExists(string nomGroupe)
+        {
+            var groupe = _context.Groupes.FirstOrDefault(x => x.NOM_GROUPE== nomGroupe);
+            if (groupe != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RoleExists(int numeroArtiste, string nomGroupe)
+        {
+            var role1 = _context.Roles.FirstOrDefault(x => x.NUMERO_ARTISTE == numeroArtiste);
+            var role2 = _context.Roles.FirstOrDefault(x => x.NOM_GROUPE == nomGroupe);
+            if (role1 != null && role2 != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ContratExists(int numeroContrat)
+        {
+            var contrat = _context.Contrats.FirstOrDefault(x => x.NUMERO_CONTRAT == numeroContrat);
+            if (contrat != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool FactureExists(int numeroFacture)
+        {
+            var facture = _context.Factures.FirstOrDefault(x => x.NUMERO_FACTURE == numeroFacture);
+            if (facture != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
