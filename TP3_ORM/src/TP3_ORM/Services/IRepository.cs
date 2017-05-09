544542058839f4176a7c5a54a81d290b4ee3d49e
Rémikya <remikya.hellal@gmail.com>
2017-05-09 15:58:50 -0500

    add interface repository

--- TP3_ORM/src/TP3_ORM/Services/IRepository.cs
+++ TP3_ORM/src/TP3_ORM/Services/IRepository.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TP3_ORM.Entities;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TP3_ORM.Services
 {
-    public class IRepository : Controller
+    public  interface IRepository 
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+        void Add(Entity myEntity);
+        void Update(Entity myEntity);
+        void Delete(Entity myEntity);
+        void Cascade();
+
+
     }
 }
